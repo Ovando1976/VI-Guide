7dc129622b119d39a8697991144ecf29148612ab
Ovando1976 <128008913+Ovando1976@users.noreply.github.com>
2026-07-20 20:20:44 -0400

    Add governors timeline page

---
+++ app/heritage/governors/page.tsx
@@ -0,0 +1,11 @@
+import GovernorTimelineExplorer from "@/components/heritage/governor-timeline-explorer";
+
+export const metadata = {
+  title: "Governors of the Virgin Islands | VI Guide",
+  description:
+    "A complete chronology of governors and administrations in the Virgin Islands from Danish company rule to elected government.",
+};
+
+export default function GovernorsTimelinePage() {
+  return <GovernorTimelineExplorer />;
+}
